23e598a5e487efc7d63bf34443017f5bf5f1d1bd
Matthew Lavine <matt@matthewlavine.net>
2024-11-10 20:40:47 -0500

    Dereference for logging

--- scheduler.go
+++ scheduler.go
@@ -142,12 +142,12 @@ func clearScreen() {
 func (s *Scheduler) LogProgress() {
 	clearScreen()
 	for _, task := range s.tasks {
-		task := task
+		task := *task
 		progress := float64(task.initialWork-task.work) / float64(task.initialWork) * 100
 		fmt.Printf("Task %d: %05.2f%% complete, progress %d/%d\n", task.id, progress, task.initialWork-task.work, task.initialWork)
 	}
 	for _, core := range s.cores {
-		core := core
+		core := *core
 		switch core.state {
 		case Idle:
 			fmt.Printf("Core %d: Idle\n", core.id)
